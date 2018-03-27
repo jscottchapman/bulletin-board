60c925f2dd0288282a7530cdbec6dfb21b8a60a6
jscottchapman <schapman@stratusdata.com>
2018-03-27 08:40:58 -0600

    MVP :lipstick:

--- src/index.js
+++ src/index.js
@@ -4,5 +4,5 @@ import './index.css';
 import Board from './board';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<Board />, document.getElementById('root'));
+ReactDOM.render(<Board count={50} />, document.getElementById('root'));
 registerServiceWorker();
